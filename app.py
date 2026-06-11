3e20162287a97e357083496e465b5b675f3785b3
Phanindra Kuntamukkala <phanindrakuntamukkala@gmail.com>
2026-06-11 14:32:03 +0530

    Initial commit - Netflix Recommender

---
+++ app.py
@@ -0,0 +1,8 @@
+def load_css():
+    with open("styles/style.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+        )
+
+load_css()
